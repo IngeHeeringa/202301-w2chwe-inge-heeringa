a98c1dbc0cd39b66bea90fa8087f298a5eb95ffd
Inge Heeringa <inge.heeringa@gmail.com>
2023-01-22 22:04:24 +0100

    Modify HTML class names

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import initializeGeneration from "./initializeGeneration/initializeGeneration.js
 import World from "./World/World.js";
 
 const rows = 20;
-const columns = 20;
+const columns = 35;
 const initialGeneration = initializeGeneration(rows, columns, true);
 const world = new World(rows, columns, initialGeneration);
 
